8cc5dcf75b95f4432b662031e2042a12de67f3bc
Richard Salazar <richard.salazar2@g.austincc.edu>
2024-01-28 20:26:17 -0600

    Updated to include RectangleInterface class which contains virtual methods in the form of an empty desctructor, bool set, double getLength, and double getWidth.

--- RectangleInterface.h
+++ RectangleInterface.h
@@ -1,1 +1,29 @@
-/** RectangleInterface Code*/
+/**
+Programmer: Richard Salazar
+Assignment: 1
+Description: Rectangle Interface
+*/
+
+#ifdef RECTANGLE_INTERFACE_
+#define RECTANGLE_INTERFACE_
+
+class RectangleInterface {
+
+public:
+
+    //Destructor
+    virtual ~RectangleInterface(){
+
+    }
+
+    // Setter
+    virtual bool set(double newLength, double newWidth) = 0;
+
+    // Getters
+    virtual double getLength() const = 0;
+    virtual double getWidth() const = 0;
+
+
+};
+
+#endif
