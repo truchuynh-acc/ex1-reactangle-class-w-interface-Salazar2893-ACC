aec207267bf874ed3d62eb823a1250c9e2c37afe
Richard Salazar <richard.salazar2@g.austincc.edu>
2024-01-28 20:10:51 -0600

    Initial Commit

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -1,1 +1,42 @@
-/**Rectangle.cpp Code*/
+/**
+Programmer: Richard Salazar
+Assignment: 1
+Description: Rectangle Class that contains length and width of rectangle
+with methods that will retrieve new length/width measurements and update
+the previous measurements and then calculate the rectangle area.
+*/
+
+#include "Rectangle.h"
+
+// Constructor
+Rectangle::Rectangle() {
+    length = 1.0;
+    width = 1.0;
+}
+
+// Setters
+bool Rectangle::set(double tempLength, tempWidth){
+
+    if (tempLength > 0.0 && tempWidth > 0.0){
+        length = tempLength;
+        width = tempWidth;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Getters
+double Rectangle::getLength() const {
+
+    return length;
+}
+
+double Rectangle::getWidth() const {
+
+    return width;
+}
+
+double Rectangle::getArea() const {
+    return length * width;
+}
--- Rectangle.h
+++ Rectangle.h
@@ -1,1 +1,34 @@
-/**Rectangle.h Code*/
+/**
+Programmer: Richard Salazar
+Assignment: 1
+Description: Header File
+*/
+
+#ifndef RECTANGLE_
+#define RECTANGLE_
+
+#include "RectangleInterface.h"
+
+class Rectangle : public RectangleInterface {
+public:
+
+    // Constructor
+    Rectangle();
+
+    // Setter
+    bool set(double, double);
+
+    // Getters
+    double getLength();
+    double getWidth();
+    double getArea();
+
+private:
+    double length;
+    double width;
+
+
+};
+
+
+#endif
