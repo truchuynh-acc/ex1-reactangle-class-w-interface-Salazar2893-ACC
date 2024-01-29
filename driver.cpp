9a03ed668259f510957d27fa0d8a75d596734585
Richard Salazar <richard.salazar2@g.austincc.edu>
2024-01-28 19:45:21 -0600

    Updated main() to create myRectangle object, accept new rectangle measurements, and formatted program output.

--- driver.cpp
+++ driver.cpp
@@ -1,9 +1,29 @@
-/**Driver Code*/
-
 /**
-Programmer: 
-Project number: 1
-Project Desc: Quadratic Expression class
-Course: COSC-2436-007 (92905) III Data Structures
-Date:   
+Programmer: Richard Salazar
+Assignment: 1
+Description: This program contains a rectangle class that will store the length and width
+of a rectangle and calculate the area.
+Course: COSC 2436 PF III Data Structures
+1/28/24
 */
+
+#include "Rectangle.h"
+
+int main(){
+
+    Rectangle myRectangle;
+
+    std::cout << "Rectangle's Base Measurements" << std::endl;
+    std::cout << "Length: " << myRectangle.getLength() << std::endl;
+    std::cout << "Width: " << myRectangle.getWidth() << std::endl;
+
+    myRectangle.setLength(10);
+    myRectangle.setWidth(5);
+    std::cout << "Rectangle's Updated Measurements" << std::endl;
+    std::cout << "Length: " << myRectangle.getLength() << std::endl;
+    std::cout << "Width: " << myRectangle.getWidth() << std::endl;
+    std::cout << "Area: " << myRectangle.getArea() << std::endl;
+
+    return 0;
+
+}
